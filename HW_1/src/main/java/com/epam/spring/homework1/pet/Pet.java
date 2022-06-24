96ad13cb157537187e1b43a6f76a48bf1f5cd36e
Oleksandr Bushyn <oleksandr_bushyn@epam.com>
2022-06-24 15:06:10 +0300

    HW 1.  spring core part 1

---
+++ HW_1/src/main/java/com/epam/spring/homework1/pet/Pet.java
@@ -0,0 +1,23 @@
+package com.epam.spring.homework1.pet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+import java.util.List;
+
+@Component
+public class Pet {
+
+    List<Animal> animals;
+    @Autowired
+    public Pet(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void printPet() {
+        for (Animal animal : animals) {
+            System.out.println(animal.toString());
+        }
+    }
+}
