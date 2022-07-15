995c90f272c40befaa2b2b92dc2c6f86081caf8b
SashaBushyn <94682150+SashaBushyn@users.noreply.github.com>
2022-07-15 19:40:04 +0300

    Delete HW_3/src/main/java/com/epam/homework3 directory

--- HW_3/src/main/java/com/epam/homework3/model/enams/OfferStatus.java
+++
@@ -1,5 +0,0 @@
-package com.epam.homework3.model.enams;
-
-public enum OfferStatus {
-    INPROCESS, ACCEPT, DECLINE,
-}
--- HW_3/src/main/java/com/epam/homework3/model/enams/OrderHandling.java
+++
@@ -1,5 +0,0 @@
-package com.epam.homework3.model.enams;
-
-public enum OrderHandling {
-    INPROCESS,ACCEPTED, DECLINED,
-}
